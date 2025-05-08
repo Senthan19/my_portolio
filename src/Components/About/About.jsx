d5a253d741c8b36b0e4e1dac68395bdc48e3b409
Senthan <senthanganesh5885@gmail.com>
2025-05-08 22:27:35 +0530

    update

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
                 <NameHeading style={{marginBottom:"20px"}}>Hi I am Senthan S</NameHeading>
                 <TypingEffect/>
                 <SelfContent style={{marginTop:"20px", marginBottom:"20px"}}>I am a passionate Software Developer with a strong foundation in Java, Spring Boot, and Full-Stack Development. I love solving problems, building scalable applications, and continuously expanding my technical expertise. With a keen interest in backend development, microservices, and cloud technologies, I aim to create impactful and efficient solutions.</SelfContent>
-                <ResumeLink href="https://drive.google.com/file/d/1Cu95Rg3pUrR80HR06e7NKgcZZnWRzkof/view?usp=drive_link" target="_blank">Check Resume</ResumeLink>
+                <ResumeLink href="https://drive.google.com/file/d/1odhmzGIQFRXk-VTa5Y9xN2JNdADy0Vuj/view?usp=drive_link" target="_blank">Check Resume</ResumeLink>
             </AboutContent>
             <ProfileIamge src={myImage} alt="my img"/>
         </AboutSection>
